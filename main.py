b9d12ba4d1a779c7e003e2db59e4606506f7669e
remo5000 <vigneshshankar@gmail.com>
2018-06-27 00:58:28 +0800

    Add getPanoramaAt function with saving

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 import urllib.request
-import key from API_KEY
+import json
+from PIL import Image
+from API_KEY import key
 
 metadata_endpoint = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
 image_endpoint = 'https://maps.googleapis.com/maps/api/streetview?'
+
+def getPanoramaAt(lat, long):
+    url = metadata_endpoint + f'location={lat},{long}&key={key}'
+    response = urllib.request.urlopen(url).read()
+    image_available = json.loads(response).get('status') == 'OK'
+    if not image_available:
+        print(f'image not available at {lat}, {long}')
+        return
+    for heading in range(0, 360, 90):
+        url = image_endpoint + f'size=640x640&location={lat},{long}&heading={heading}&fov=90&key={key}'
+        img =  Image.open(urllib.request.urlopen(url))
+        img.save("asdf.jpeg")
+
+getPanoramaAt(40.457375,-80.009353)
 
