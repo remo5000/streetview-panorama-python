ae8acbe33ef0b768428582dbfc9d8b7b716dbcb7
remo5000 <vigneshshankar@gmail.com>
2018-06-27 00:20:08 +0800

    Add files with endpoint

---
+++ main.py
@@ -0,0 +1,6 @@
+import urllib.request
+import key from API_KEY
+
+metadata_endpoint = 'https://maps.googleapis.com/maps/api/streetview/metadata?'
+image_endpoint = 'https://maps.googleapis.com/maps/api/streetview?'
+
